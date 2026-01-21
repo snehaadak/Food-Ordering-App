22b47d884ca03dd36da1cfe3db1ccb9fe00e8291
Sneha <snehaadak321@gmail.com>
2026-01-21 22:53:12 +0530

    Basic File creation Commits

---
+++ App.js
@@ -0,0 +1,4 @@
+const heading = document.createElement("h1");
+            heading.innerHTML = "Hello World! from Javascript";
+            const root1 = document.getElementById("root1");
+            root1.appendChild(heading)
