685b0c409aa15747ac676b35615254f90abf7ab5
Sneha <snehaadak321@gmail.com>
2026-01-24 22:52:17 +0530

    Setting up Parcel and React

--- App.js
+++ App.js
@@ -1,9 +1,23 @@
-const parent = React.createElement("div",{id:parentElement},
-    React.createElement("div",{id:chilElment},[
-        React.createElement("h2",{},Child1),
-        React.createElement("h2",{},Child2)  
-    ]
-))
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const heading2 = React.createElement(
+    "h2",
+    {id: "heading"},
+    "Hello, from React!");
+console.log(heading2); 
 const root2 = ReactDOM.createRoot(document.getElementById("root2"))
-root2.render(parent)
+root2.render(heading2)
+
+
+const parent = React.createElement("div", {id: "parent" },
+    React.createElement("div", {id:"child"} ,
+        [React.createElement("h1",{},"I'm an h1 Tag"),
+         React.createElement("h2",{},"I'm an h2 Tag")  
+        ]
+    )
+);
+const root3 = ReactDOM.createRoot(document.getElementById("root"))
+root3.render(parent);
