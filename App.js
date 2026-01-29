8f7945fcd2b7d04877b1d5815dc76e4d2e3b05fe
Sneha <snehaadak321@gmail.com>
2026-01-29 23:00:06 +0530

    About JSX

--- App.js
+++ App.js
@@ -10,13 +10,19 @@ console.log(heading2);
 const root2 = ReactDOM.createRoot(document.getElementById("root2"))
 root2.render(heading2)
 
+// Simple React code without JSX
+// const parent = React.createElement("div", {id: "parent" },
+//     React.createElement("div", {id:"child"} ,
+//         [React.createElement("h1",{},"I'm an h1 Tag"),
+//          React.createElement("h2",{},"I'm an h2 Tag")  
+//         ]
+//     )
+// );
 
-const parent = React.createElement("div", {id: "parent" },
-    React.createElement("div", {id:"child"} ,
-        [React.createElement("h1",{},"I'm an h1 Tag"),
-         React.createElement("h2",{},"I'm an h2 Tag")  
-        ]
-    )
-);
+//Basic JSX code
+const jsxHeading = <h1>Hello From JSX</h1>
+
+
+//IMP part for rendering our whole piece of code 
 const root3 = ReactDOM.createRoot(document.getElementById("root"))
-root3.render(parent);
+root3.render(jsxHeading);
