3efe085880f38d1b305bba3a64e99f700433bfe3
Sneha <snehaadak321@gmail.com>
2026-01-30 23:45:38 +0530

    Webpage Basic Stucture

--- App.js
+++ App.js
@@ -1,42 +1,62 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
-const heading2 = React.createElement(
-    "h2",
-    {id: "heading"},
-    "Hello, from React!");
-console.log(heading2); 
-const root2 = ReactDOM.createRoot(document.getElementById("root2"))
-root2.render(heading2)
-
-//Simple React code without JSX
-// const parent = React.createElement("div", {id: "parent" },
-//     React.createElement("div", {id:"child"} ,
-//         [React.createElement("h1",{},"I'm an h1 Tag"),
-//          React.createElement("h2",{},"I'm an h2 Tag")  
-//         ]
-//     )
-// );
-
-//Basic JSX code
-const jsxHeading = <h2>Hello From JSX</h2>
-
-const FunctionalComponentHeading = () => {
-    return <h2>This is a Functional Component</h2>
-}
-
-const ComponentComposition = () => {
+const SearchBar = () => {
     return (
         <div>
-            <h1>This is Component Composition</h1>
-            {jsxHeading}
-            {FunctionalComponentHeading()}
-            <FunctionalComponentHeading/>
-        </div> )
+            <h5>Search Bar</h5>
+        </div>
+    )
+}
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="Logo">
+                <h1>logo</h1>
+            </div>
+            <div className="searchBar">
+                <SearchBar/>
+            </div>
+            <div className="navItems">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>   
+        </div>
+    )
+}
+
+const RestroCard = () =>{
+    return (
+        <div className="RestroCard">
+            <h3>Dominos</h3>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="resContainer">
+                <RestroCard/>
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header/>
+            <Body/>
+        </div>
+    )
 }
 
 
 //IMP part for rendering our whole piece of code 
 const root3 = ReactDOM.createRoot(document.getElementById("root"))
-root3.render(<ComponentComposition/>);
+root3.render(<AppLayout/>);
