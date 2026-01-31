22b3c1c1a5b0201320f91725d791bc45a73dfe20
Sneha <snehaadak321@gmail.com>
2026-01-31 23:53:51 +0530

    Passing Data throughProps

--- App.js
+++ App.js
@@ -31,15 +31,15 @@ const Header = () => {
     )
 }
 
-const RestroCard = () =>{
+const RestroCard = (props) =>{
     return (
         <div className="RestroCard">
             <img className="restroBanner" alt="restroBanner"
             src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/11/11/cbf23535-b379-4d95-a0ca-32b06b268a13_25459.JPG"></img>
-            <h3>Restro Name</h3>
-            <h4>Cuisines</h4>
-            <h5>Ratings</h5>
-            <h5>ETA</h5>
+            <h3>{props.resName}</h3>
+            <h4>{props.cuisines}</h4>
+            <h5>{props.ratings}</h5>
+            <h5>{props.eta}</h5>
         </div>
     )
 }
@@ -48,16 +48,11 @@ const Body = () => {
     return (
         <div className="body">
             <div className="resContainer">
-                <RestroCard/>
-                <RestroCard/>
-                <RestroCard/>
-                <RestroCard/>
-                <RestroCard/>
-                <RestroCard/>
-                <RestroCard/>
-                <RestroCard/>
-                <RestroCard/>
-                <RestroCard/>
+                <RestroCard 
+                resName="Dominos" 
+                cuisines="Italian, Pizzas, Sandwiches" 
+                ratings="4.3" 
+                eta="25-30 mins"/>
             </div>
         </div>
     )
