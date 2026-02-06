f09d440337092cc9d4ad7d0839a012bf80a42a12
Sneha <snehaadak321@gmail.com>
2026-02-07 00:34:39 +0530

    Simple useState example for Login-Logout UI rendering

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,17 +4,12 @@ import  {useEffect, useState} from "react";
 import { ShimmerUI } from "./ShimmerUI";
 
 
-const SearchBar = () => {
-    return (
-        <div>
-            <h5>Search Bar</h5>
-        </div>
-    )
-}
 
 const Body = () => {
 
     const [listofRestros,setlistofRestros] = useState([]);
+    const [filteredResturants, setfilterRestuarants] = useState([])
+    const [searchText, setsearchText] = useState("")
 
     useEffect(()=>{
         fetchData();
@@ -24,7 +19,8 @@ const Body = () => {
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5539311&lng=73.9476257&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json = await data.json();
 
-  setlistofRestros(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+        setlistofRestros(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+        setfilterRestuarants(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants)
     }
 
     if(listofRestros.length === 0){
@@ -35,19 +31,14 @@ const Body = () => {
         <div className="body">
             <div className="FilterContainer">
                 <div>
-                    <button 
-                        className="TopRatedBTN"
+                    <button className="TopRatedBTN"
                         onClick={()=>{
                             const filteredList = listofRestros.filter(
-                                (res) => res.info.avgRating > 4.2
-                            );
-                            setlistofRestros(filteredList)
-                        }}
-                    >Top Rated Restuarants</button>
+                                (res) => res.info.avgRating > 4.2);
+                                setlistofRestros(filteredList)
+                            }}>Top Rated Restuarants</button>
                 </div>
-                <div className="searchBar">
-                <SearchBar/>
-                </div>
+                
             </div>
             
             <h3>What's On Your Mind?</h3>
@@ -61,7 +52,7 @@ const Body = () => {
             <div className="restro-Container">
 
                 {
-                    listofRestros.map(restuarant => <RestroCard key={restuarant.info.id} resData = {restuarant}/>)
+                    filteredResturants.map(restuarant => <RestroCard key={restuarant.info.id} resData = {restuarant}/>)
                 }
             </div>
             
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import AppLogo from "../utils/AppLogo.png"
 
 const Header = () => {
+
+ const [loginButton, setloginButton] = useState("Login")
+
+
     return (
         <div className="header">
             <div className="Logo">
@@ -17,6 +22,12 @@ const Header = () => {
                     <li>About Us</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+                    <button className="login" onClick={()=>{
+                        loginButton === "Login" ? 
+                        setloginButton("Logout"):
+                        setloginButton("Login")}
+                    }
+                        >{loginButton}</button>
                 </ul>
             </div>   
         </div>
